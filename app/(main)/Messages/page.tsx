98c06fdc7bf467b73d604cb3e62859e23f344e20
Ebraheem hany <ebraheem200412345@gmail.com>
2026-04-30 15:25:47 +0300

    fix type script error

--- app/(main)/Messages/page.tsx
+++ app/(main)/Messages/page.tsx
@@ -1,6 +1,7 @@
+import MessagesClient from "@/component/items/MessagesClient";
 import { Suspense } from "react";
-import MessagesClient from "./MessagesClient";
 
+export const dynamic = "force-dynamic";
 export default function MessagesPage() {
   return (
     <Suspense
