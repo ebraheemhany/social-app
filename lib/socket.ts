08e38d70c656fc1f7853fc30f1a760db04f14274
ebraheem hany <ebraheemhany2004@gmail.com>
2026-06-09 15:30:06 +0300

    fix socket.io

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,6 +1,6 @@
-import  io  from "socket.io-client";
+import io from "socket.io-client";
 
-const socket = io(process.env.NEXT_PUBLIC_API_URL!, {
+const socket = io(process.env.NEXT_PUBLIC_BASE_API_URL!, {
   autoConnect: true,
 });
 
